45150d29f8d68006ea99d509407f0df5fa6fcad3
Daniel Huchani <161368230+HuchaniDev@users.noreply.github.com>
2025-03-21 02:00:41 -0400

    feat: enhance strategic axis navigation and layout adjustments

--- src/app/features/main-layout/views/main-dashboard/main-dashboard.component.ts
+++ src/app/features/main-layout/views/main-dashboard/main-dashboard.component.ts
@@ -1,6 +1,4 @@
 import { Component, inject } from "@angular/core";
-import authService from "../../../../core/services/auth.service";
-import { Feature } from "../../models/features";
 import PermissionService from "../../../../core/services/permission.service";
 import { Router } from "@angular/router";
 
@@ -16,58 +14,6 @@ export default class MainDashboardComponent {
  constructor() {
 
  }
-
-  features: Feature[] = [
-    {
-      name: 'Users',
-      route: '/users',
-      icon: 'people',
-      color: '',
-      roles: ['Admin']
-    },
-    {
-      name: 'Gestiones',
-      route: '/management',
-      icon: 'settings',
-      color: '',
-      roles: ['Admin']
-    },
-    {
-      name: 'Unidad',
-      route: '/unit',
-      icon: 'settings',
-      color: '',
-      roles: ['Admin']
-    },
-    {
-      name: 'Funionarios',
-      route: '/official',
-      icon: 'settings',
-      color: '',
-      roles: ['Admin']
-    },
-    {
-      name: 'Reportes',
-      route: '/report',
-      icon: 'settings',
-      color: '',
-      roles: ['Admin','Planificador']
-    },
-    {
-      name: 'Eje Estrategico',
-      route: '/strategic-axis',
-      icon: 'settings',
-      color: '',
-      roles: ['Admin']
-    },
-    {
-      name: 'Poas',
-      route: '/poa',
-      icon: 'settings',
-      color: '',
-      roles: ['Director','Planificador']
-    },
-  ];
 
   navigateTo(route: string) {
     this.#routes.navigate([route]);
--- src/app/features/main-layout/views/strategic-line/index/strategic-line-index.component.ts
+++ src/app/features/main-layout/views/strategic-line/index/strategic-line-index.component.ts
@@ -1,9 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { PageHeaderComponent } from "../../../../../shared/controls/page-header/page-header.component";
 
 @Component({
     selector:'app-strategic-line',
     templateUrl:'./strategic-line-index.component.html',
     standalone:true,
-    imports:[]
+    imports: [PageHeaderComponent]
 })
-export default class StrategicLineComponent{}
+export default class StrategicLineComponent{
+	#router = inject(Router);
+
+	strateAxisId:number | null = null;
+
+	constructor() {
+		this.strateAxisId = history.state;
+
+		if(this.strateAxisId && history.state.navigationId){
+
+		}
+			
+	}
+
+	returnToAxis(){
+		this.#router.navigate(['strategic-axis']);
+	}
+}
